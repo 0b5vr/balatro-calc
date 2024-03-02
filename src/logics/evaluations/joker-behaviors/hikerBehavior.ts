8ca8814f56456f2f3a0674099109bb9f4ce6ead8
0b5vr <0b5vr@0b5vr.com>
2024-03-02 16:11:08 +0900

    fix: Fix upgrade timing of hiker

--- src/logics/evaluations/joker-behaviors/hikerBehavior.ts
+++ src/logics/evaluations/joker-behaviors/hikerBehavior.ts
@@ -4,11 +4,9 @@ import { JokerBehavior } from './JokerBehavior';
 export const hikerBehavior: JokerBehavior = {
   displayName: 'Hiker',
   rarity: 'Uncommon',
-  onBeforeEvaluate(result, _board, joker) {
-    for (const card of result.handResult!.scored) {
-      const subject = `${card.toDisplayString()} (${joker.toDisplayString()})`;
-      card.extraChips += 4;
-      resultAddLog(result, subject, `Extra chips +${card.extraChips}`);
-    }
+  onCardTriggered(result, _board, joker, scored) {
+    const subject = `${scored.toDisplayString()} (${joker.toDisplayString()})`;
+    scored.extraChips += 4;
+    resultAddLog(result, subject, `Upgrade! (+${scored.extraChips})`);
   },
 };
